9d94336ffd4984d511b8ca98bac92e8b8de782d4
Jandyle A. Gonzales <jandyle.gonzales@truste.com>
2021-08-31 14:12:37 +0800

    Tests

--- src/main/java/com/example/springSample/SpringSampleApplication.java
+++ src/main/java/com/example/springSample/SpringSampleApplication.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SpringSampleApplication extends SpringBootServletInitializer {
 
 	@Override
@@ -18,16 +15,4 @@ public class SpringSampleApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSampleApplication.class, args);
 	}
-
-	@RequestMapping(value = "/")
-	public String hello(){
-		return "Hello World";
-	}
-
-	@RequestMapping(value = "/too")
-	public String pap(){
-		return "wp";
-	}
-
-
 }
---
+++ src/main/java/com/example/springSample/controller/StudentController.java
@@ -0,0 +1,62 @@
+package com.example.springSample.controller;
+
+
+import com.example.springSample.model.Course;
+import com.example.springSample.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+	@Autowired
+	private StudentService studentService;
+
+	@GetMapping("/students/{studentId}/courses")
+	public List<Course> retrieveCoursesForStudent(@PathVariable String studentId) {
+		return studentService.retrieveCourses(studentId);
+	}
+
+	@PostMapping("/students/{studentId}/courses")
+	public ResponseEntity<Void> registerStudentForCourse(
+			@PathVariable String studentId, @RequestBody Course newCourse) {
+
+		Course course = studentService.addCourse(studentId, newCourse);
+
+		if (course == null)
+			return ResponseEntity.noContent().build();
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path(
+				"/{id}").buildAndExpand(course.getId()).toUri();
+
+		return ResponseEntity.created(location).build();
+	}
+
+	@GetMapping("/students/{studentId}/courses/{courseId}")
+	public Course retrieveDetailsForCourse(@PathVariable String studentId,
+			@PathVariable String courseId) {
+		return studentService.retrieveCourse(studentId, courseId);
+	}
+
+
+	@RequestMapping(value = "/")
+	public String hello(){
+		return "Pikachu";
+	}
+
+	@RequestMapping(value = "/too")
+	public String pap() {
+		return "wp";
+	}
+
+}
